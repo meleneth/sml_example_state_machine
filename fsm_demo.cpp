88bcceeff5e48e82fb2a080ca9dba20d5d46b215
Meleneth <meleneth@gmail.com>
2025-05-31 23:02:04 -0700

    Get on_entry and on_exit working

--- fsm_demo.cpp
+++ fsm_demo.cpp
@@ -3,6 +3,12 @@
 
 namespace sml = boost::sml;
 
+#define SML_ENTRY_FN(name, body)            \
+  struct name {                             \
+    template <class E, class F, class S, class T> \
+    void operator()(const E&, F&, S&, T&) const body \
+  }
+
 // Events
 struct play {};
 struct pause {};
@@ -21,25 +27,21 @@ auto do_stop = [] { std::puts("[action] Stopping"); };
 auto do_reset = [] { std::puts("[action] Resetting"); };
 
 // States with entry/exit actions
-struct Stopped {
-    auto on_entry() const { std::puts("[enter] Stopped"); }
-    auto on_exit() const  { std::puts("[exit] Stopped"); }
-};
+struct Stopped {};
+SML_ENTRY_FN(Stopped_on_entry, { std::puts("[enter] Stopped"); });
+SML_ENTRY_FN(Stopped_on_exit, { std::puts("[exit] Stopped"); });
 
-struct Playing {
-    auto on_entry() const { std::puts("[enter] Playing"); }
-    auto on_exit() const  { std::puts("[exit] Playing"); }
-};
+struct Playing {};
+SML_ENTRY_FN(Playing_on_entry, { std::puts("[enter] Playing"); });
+SML_ENTRY_FN(Playing_on_exit, { std::puts("[exit] Playing"); });
 
-struct Paused {
-    auto on_entry() const { std::puts("[enter] Paused"); }
-    auto on_exit() const  { std::puts("[exit] Paused"); }
-};
+struct Paused {};
+SML_ENTRY_FN(Paused_on_entry, { std::puts("[enter] Paused"); });
+SML_ENTRY_FN(Paused_on_exit, { std::puts("[exit] Paused"); });
 
-struct Error {
-    auto on_entry() const { std::puts("[enter] Error"); }
-    auto on_exit() const  { std::puts("[exit] Error"); }
-};
+struct Error {};
+SML_ENTRY_FN(Error_on_entry, { std::puts("[enter] Error"); });
+SML_ENTRY_FN(Error_on_exit, { std::puts("[exit] Error"); });
 
 // Transition table
 struct player {
@@ -51,7 +53,15 @@ struct player {
              state<Paused>  + event<play>           / do_play  = state<Playing>,
              state<Playing> + event<stop>           / do_stop  = state<Stopped>,
              state<Playing> + event<error_occurred> / []{ std::puts("[error] Transitioning to Error"); } = state<Error>,
-             state<Error>   + event<reset>          / do_reset = state<Stopped>
+             state<Error>   + event<reset>          / do_reset = state<Stopped>,
+             state<Stopped> + on_entry<_> / Stopped_on_entry{},
+             state<Stopped> + boost::sml::on_exit<_> / Stopped_on_exit{},
+             state<Playing> + on_entry<_> / Playing_on_entry{},
+             state<Playing> + boost::sml::on_exit<_> / Playing_on_exit{},
+             state<Paused> + on_entry<_> / Paused_on_entry{},
+             state<Paused> + boost::sml::on_exit<_> / Paused_on_exit{},
+             state<Error> + on_entry<_> / Error_on_entry{},
+             state<Error> + boost::sml::on_exit<_> / Error_on_exit{}
         );
     }
 };
