53551654133d823a8e590bfa77cd671d46bae98b
Meleneth <meleneth@gmail.com>
2025-05-31 19:18:49 -0700

    Add render event to charging_fsm

--- charge_fsm.cpp
+++ charge_fsm.cpp
@@ -4,6 +4,7 @@
 namespace sml = boost::sml;
 
 struct next {};  // Event
+struct render {};  // Event
 
 // External context
 struct Context {
@@ -40,20 +41,30 @@ struct fsm_def {
             std::cout << "Transitioning to discharge\n";
         };
 
+        auto render_charging = [](const render&, Context&){
+          std::cout << "Render Charging" << std::endl;
+        };
+
+        auto render_discharge = [](const render&, Context&){
+          std::cout << "Render Discharge" << std::endl;
+        };
+
         return make_transition_table(
             *state<charging> + event<next> [ counter_ge_4 ] / transition_msg = state<discharge>,
              state<charging> + event<next> [ counter_lt_4 ] / increment      ,
-             state<discharge> + event<next> / reset                          = state<charging>
+             state<discharge> + event<next> / reset                          = state<charging>,
+             state<charging> + event<render> / render_charging,
+             state<discharge> + event<render> / render_discharge
         );
     }
 };
-
 
 int main() {
     Context ctx;
     sml::sm<fsm_def> sm{ctx};
 
     for (int i = 0; i < 10; ++i) {
+        sm.process_event(render{});
         sm.process_event(next{});
     }
 }
